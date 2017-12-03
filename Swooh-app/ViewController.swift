eb772e09ba80e2059df316bc5f58e956bc7b15b8
Karl Bullock <karlbullock@Karls-MBP.home>
2017-12-03 20:05:29 +0000

    added contraints to swoog logo

--- Swooh-app/ViewController.swift
+++ Swooh-app/ViewController.swift
@@ -10,9 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var swooh: UIImageView!
+    @IBOutlet weak var bgImg: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
