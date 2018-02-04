5960061c026c75744d2f6db8ee6a00c483147649
Karl Bullock <karlbullock@Karls-MBP.home>
2018-02-04 13:13:38 +0000

    added IBactions to leagueVC so u cant hit next button unless you select an option

--- Swooh-app/Controller/LeagueVC.swift
+++ Swooh-app/Controller/LeagueVC.swift
@@ -10,9 +10,14 @@ import UIKit
 
 class LeagueVC: UIViewController {
 
- 
+    var player: Player!
+    
+    @IBOutlet weak var nextBtn: BorderButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        player = Player()
 
         // Do any additional setup after loading the view.
     }
@@ -21,7 +26,25 @@ class LeagueVC: UIViewController {
         performSegue(withIdentifier: "skillVCSegue", sender: self)
     }
     
-
+    @IBAction func onMensTapped(_ sender: Any) {
+        selectLeague(leagueType: "mens")
+    }
+    
+    @IBAction func onWomensTapped(_ sender: Any) {
+        selectLeague(leagueType: "womans")
+    }
+    
+    @IBAction func OnCoedTapped(_ sender: Any) {
+        selectLeague(leagueType: "coed")
+    }
+    
+    
+    func selectLeague(leagueType: String) {
+        player.desiredLeague = leagueType
+        nextBtn.isEnabled = true
+    }
+    
+    
     /*
     // MARK: - Navigation
 
---
+++ Swooh-app/Model/Player.swift
@@ -0,0 +1,14 @@
+//
+//  Player.swift
+//  Swooh-app
+//
+//  Created by Karl Bullock on 03/02/2018.
+//  Copyright © 2018 Karl Bullock. All rights reserved.
+//
+
+import Foundation
+
+struct  Player {
+    var desiredLeague: String?
+    var selectedSkillLevel: String?
+}
